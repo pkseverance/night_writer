7eafe28f080bcdc6b2216ef9b373325471db04af
Patricia Severance <patriciaseverance@gmail.com>
2022-09-27 13:55:51 -0600

    Change braille_converter to inherit braille_dictionary

--- lib/braille_converter.rb
+++ lib/braille_converter.rb
@@ -1,11 +1,7 @@
 require './lib/braille_dictionary'
-class BrailleConverter
-  attr_reader :braille_dictionary
-  def initialize
-    @braille_dictionary = BrailleDictionary.new
-  end
+class BrailleConverter < BrailleDictionary
 
   def convert_to_braille(str)
-    str.downcase.chars.map {|char| @braille_dictionary.dictionary[char]}
+    str.downcase.chars.map {|char| dictionary[char]}
   end
 end
